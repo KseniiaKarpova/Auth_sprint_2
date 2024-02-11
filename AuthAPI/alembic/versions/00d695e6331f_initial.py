d2c6619720ada0ffc2695506973b366010bef915
kseniia <ksusha.karpova2011@yandex.ru>
2024-02-11 14:30:42 +0300

    add partittions

--- AuthAPI/alembic/versions/00d695e6331f_initial.py
+++ AuthAPI/alembic/versions/00d695e6331f_initial.py
@@ -44,68 +44,71 @@ def upgrade() -> None:
     sa.UniqueConstraint('email'),
     sa.UniqueConstraint('login')
     )
-    #op.create_table('user_history',
-    #sa.Column('uuid', sa.Uuid(), nullable=False),
-    #sa.Column('user_id', sa.Uuid(), nullable=False),
-    #sa.Column('user_agent', sa.String(length=255), nullable=True),
-    #sa.Column('refresh_token', sa.Text(), nullable=True),
-    #sa.Column('is_active', sa.Boolean(), nullable=False),
-    #sa.Column('created_at', sa.DateTime(), nullable=False),
-    #sa.Column('updated_at', sa.DateTime(), nullable=True),
-    #sa.ForeignKeyConstraint(['user_id'], ['users.uuid'], ),
-    #sa.PrimaryKeyConstraint('uuid')
+    op.create_table('user_history',
+    sa.Column('uuid', sa.Uuid(), nullable=False),
+    sa.Column('user_id', sa.Uuid(), nullable=False),
+    sa.Column('user_agent', sa.String(length=255), nullable=True),
+    sa.Column('refresh_token', sa.Text(), nullable=True),
+    sa.Column('is_active', sa.Boolean(), nullable=False),
+    sa.Column('created_at', sa.DateTime(), nullable=False),
+    sa.Column('updated_at', sa.DateTime(), nullable=True),
+    sa.ForeignKeyConstraint(['user_id'], ['users.uuid'], ),
+    sa.PrimaryKeyConstraint('uuid', 'user_id'),
+    sa.UniqueConstraint('uuid', 'user_id'),
+    postgresql_partition_by='HASH (user_id)'
+    )
+    #op.execute(
+    #    """CREATE TABLE user_history (
+    #        uuid uuid DEFAULT gen_random_uuid() NOT NULL,
+    #        user_id uuid NOT NULL REFERENCES users,
+    #        user_agent VARCHAR(255) NOT NULL,
+    #        refresh_token TEXT NOT NULL,
+    #        is_active boolean NOT NULL,
+    #        created_at DATE NOT NULL,
+    #        updated_at DATE,
+    #        PRIMARY KEY (uuid)
+    #    ) PARTITION BY HASH(user_id);
+    #    """
     #)
     op.execute(
-        """CREATE TABLE user_history (
-            uuid uuid DEFAULT gen_random_uuid() NOT NULL,
-            user_id uuid NOT NULL REFERENCES users,
-            user_agent VARCHAR(255) NOT NULL,
-            refresh_token TEXT NOT NULL,
-            is_active boolean NOT NULL,
-            created_at DATE NOT NULL,
-            updated_at DATE,
-            PRIMARY KEY (uuid)
-        ) PARTITION BY HASH(user_id);
-        """
-    )
-    op.execute(
-        "CREATE TABLE hash_0 PARTITION OF user_history FOR VALUES WITH (MODULES 10, REMAINDER 0);"
+        "CREATE TABLE hash_part_0 PARTITION OF user_history "
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 0);"
     )
     op.execute(
         "CREATE TABLE hash_part_1 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 1);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 1);"
     )
     op.execute(
         "CREATE TABLE hash_part_2 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 2);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 2);"
     )
     op.execute(
         "CREATE TABLE hash_part_3 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 3);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 3);"
     )
     op.execute(
         "CREATE TABLE hash_part_4 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 4);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 4);"
     )
     op.execute(
         "CREATE TABLE hash_part_5 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 5);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 5);"
     )
     op.execute(
         "CREATE TABLE hash_part_6 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 6);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 6);"
     )
     op.execute(
         "CREATE TABLE hash_part_7 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 7);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 7);"
     )
     op.execute(
         "CREATE TABLE hash_part_8 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 8);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 8);"
     )
     op.execute(
         "CREATE TABLE hash_part_9 PARTITION OF user_history "
-        "FOR VALUES WITH (MODULES 10, REMAINDER 9);"
+        "FOR VALUES WITH (MODULUS 10, REMAINDER 9);"
     )
     op.create_table('users_roles',
     sa.Column('uuid', sa.Uuid(), nullable=False),
