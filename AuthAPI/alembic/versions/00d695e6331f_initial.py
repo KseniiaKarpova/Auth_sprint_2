4fe990afcaa0825834d8c629e80d7566d43b406e
kseniia <ksusha.karpova2011@yandex.ru>
2024-02-11 19:13:52 +0300

    add partityion PG by hash user_id

--- AuthAPI/alembic/versions/00d695e6331f_initial.py
+++ AuthAPI/alembic/versions/00d695e6331f_initial.py
@@ -7,9 +7,8 @@ Create Date: 2024-01-30 12:55:40.396586
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '00d695e6331f'
