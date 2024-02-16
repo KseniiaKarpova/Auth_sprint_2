e084768705776b32879aad320eca0c78664f15f7
NozirTukhtamurodov <nozir9407@gmail.com>
2024-02-16 11:48:51 +0500

    refactoring

--- AuthAPI/core/oauth2.py
+++ AuthAPI/core/oauth2.py
@@ -1,5 +1,3 @@
-from authlib.integrations.starlette_client import OAuth
-from core import config
 from authlib.integrations.httpx_client import AsyncOAuth2Client
 
 google_client: AsyncOAuth2Client = None
