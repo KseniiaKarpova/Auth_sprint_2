efed674bf6650059ce1f18799e335f80fe997b86
NozirTukhtamurodov <nozir9407@gmail.com>
2024-02-16 11:38:09 +0500

    auth almost done

--- AuthAPI/api/v1/socials.py
+++ AuthAPI/api/v1/socials.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Query
 from starlette.requests import Request as starler_request
 from core.oauth2 import google_client
 from core.config import settings
@@ -7,7 +7,18 @@ router = APIRouter()
 
 
 @router.get('/')
-async def main_page(request: starler_request):
+async def main_page(
+        request: Request):
     uri, state = google_client.create_authorization_url(
         url=settings.auth.google_base_url)
     return uri
+
+
+@router.route('/auth')
+async def auth2(
+        request: Request,
+        code: str = Query(description='Code from auth provider'),
+    ):
+    data = await google_client.fetch_token(settings.auth.google_token_url
+                                    ,authorization_response=code)
+    return data
